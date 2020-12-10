4683776a5538a547340a5d95328bddf037bc9138
Andy Mahoney <32443674+BoneKing@users.noreply.github.com>
2020-12-10 16:53:26 -0500

    eh a whole lot of nothing

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Dungeon_Redux
         {
             Random random;
             //bool gameOver = false;
-            int numEnemies = 4; //actual num +1;
+            //int numEnemies = 4; //actual num +1;
             /*
             struct HighScore{
                 int highScore = 0;
@@ -144,7 +144,6 @@ namespace Dungeon_Redux
                         break;
 
                 }
-
             }
             Console.WriteLine("Score: {0}", p1.score);
         }
@@ -280,6 +279,7 @@ namespace Dungeon_Redux
             }
             else if(day >= 3 && day < 5){
                 if(day == 4 && hour >= 20){
+                    Console.Clear();
                     Console.WriteLine("*You see that the cave comes to a point where 20 foot steel doors stand in your way to the next area.*");
                     Console.WriteLine("*As you approach the door you hear a deep voice saying 'None shall pass through the gates of Hell without permission!'*");
                     Console.WriteLine("*Huge flames suddenly appear before the door and a giant red man draped in chains and breathing fire appears, wielding a giant hammer!*");
@@ -292,12 +292,14 @@ namespace Dungeon_Redux
             else if(day >= 5 && day < 7){
                 if(day == 6 && hour >= 20){
                     if(random.Next(1,3) == 1){
+                        Console.Clear();
                         Console.WriteLine("As you walk deeper into hell you see a 50 ft dog with 3 heads!");
                         Console.WriteLine("He paces back and forth in from of a bridge that leads even deeper into the depths of hell.");
                         Console.WriteLine("Welp, we made it this far, might as well go farther.");
                         index = 17;
                     }
                     else{
+                        Console.Clear();
                         Console.WriteLine("As you walk deeper into hell you see a 50 ft dog with 3 heads!");
                         Console.WriteLine("He paces back and forth in front of a bridge that leads even deeper into the depths of hell.");
                         Console.WriteLine("Welp, we made it this far, might as well go farther.");
@@ -312,7 +314,8 @@ namespace Dungeon_Redux
                 }
             }
             else if(day > 6){
-                if(day == 7 && hour >= 20){
+                if((day == 7 && hour >= 20) || day > 7){
+                    Console.Clear();
                     Console.WriteLine("You see off in the distance a demonic castle.");
                     Console.WriteLine("You run to it bursting through the gran1d doors into the main hall");
                     Console.WriteLine("There on a throne you see a huge dark demon king with a crown");
@@ -434,6 +437,7 @@ namespace Dungeon_Redux
             if(p1.health < 1){
                 return;
             }
+            Console.Clear();
             Console.WriteLine("Chad falls to the floor, having used all of his energy he turns to chrimson dust and blows away");
             Console.WriteLine("Congrats, you are now the king of Hell.");
             Console.WriteLine("#############################################");
